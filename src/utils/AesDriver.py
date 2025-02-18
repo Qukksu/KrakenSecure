b9689c7c1d86e0f9323ab7c3ce0377dc24f52cdb
_Dms <dsrmy1221@gmail.com>
2025-02-18 11:54:41 +1000

    added binary encrypted_AES_mac Upd: Aes driver bp

--- src/utils/AesDriver.py
+++ src/utils/AesDriver.py
@@ -3,13 +3,18 @@ import subprocess
 
 
 class AesDriver:
-    def __init__(self):
-        self._dir = "../utils/encrypted_aes"
+    def __init__(self, aes_file_path="../utils/encrypted_aes_mac"):
+        self._dir = aes_file_path
         if not os.path.exists(self._dir):
             raise Exception(f"File {self._dir} not found")
 
-    def encrypt(self, input_file: str, output_file: str, passphrase: str) -> None:
-        subprocess.run([self._dir, "encrypt", input_file, output_file, passphrase])
+    def encrypt(self, input_file: str, output_file: str, passphrase: str) -> int:
+        result = subprocess.run([self._dir, "encrypt", input_file, output_file, passphrase], capture_output=True,
+                                text=True)
+        return result.returncode
 
-    def decrypt(self, input_file: str, output_file: str, passphrase: str) -> None:
-        subprocess.run([self._dir, "decrypt", input_file, output_file, passphrase])
+    def decrypt(self, input_file: str, output_file: str, passphrase: str) -> int:
+        result = subprocess.run([self._dir, "decrypt", input_file, output_file, passphrase], capture_output=True,
+                                text=True)
+        print(result.stderr)
+        return result.returncode
